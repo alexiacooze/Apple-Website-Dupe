791b2730e9e0588500e78b9d4b4e26175720c901
Alexia Cooze <alexiacooze@Alexias-MacBook-Air.local>
2022-07-13 12:58:55 -0400

    finished the design for the navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
+import "../Header/Header.scss"
+import logo from "../../assets/logo.png"
+import search from "../../assets/search.png"
+import purchase from "../../assets/purchase.png"
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header__container">
-        {/* <div className="header__logo-container">
+        <div className="header__logo-container">
             <img className="header__logo" src={logo} alt="Page Logo" />
 
-        </div> */}
+        </div>
         <nav className="header__nav-container">
           <ul className="header__menu">
             <li className="header__store">Store</li>
@@ -20,6 +24,8 @@ export default function Header() {
             <li className="header__apple-only">Only on Apple</li>
             <li className="header__accessories">Accessories</li>
             <li className="header__support">Support</li>
+            <img  className="header__search" src={search} alt="search"/>
+            <img className="header__purchase"  src={purchase} alt="purchase"/>
           </ul>
         </nav>
       </div>
