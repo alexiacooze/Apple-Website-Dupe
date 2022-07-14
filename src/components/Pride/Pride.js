aec67050ef802b9e43d6c9f53d5d6ab7850045fa
Alexia Cooze <alexiacooze@Alexias-MacBook-Air.local>
2022-07-13 23:18:04 -0400

    finished styling the pride component

--- src/components/Pride/Pride.js
+++ src/components/Pride/Pride.js
@@ -1,15 +1,17 @@
 import React from "react";
+import "../Pride/Pride.scss"
+import threeWatches from "../../assets/three-watches.png"
 
 export default function Pride() {
   return (
     <section className="pride">
       <div className="pride__header-container">
         <p className="pride__header-text">Threads of</p>
-        <img className="pride__header-image" alt=""></img>
+        <img className="pride__header-image" src="https://www.apple.com/v/watch/ax/images/overview/pride/logo_pride_static__d6k5drslkiie_large_2x.jpg" alt=""></img>
         <p className="pride__sub-header-text">
           Celebrate all year long with new Pride Edition watch bands and faces.
         </p>
-        <img alt=""></img>
+        <img src={threeWatches} alt=""></img>
       </div>
       <div className="pride__double-container">
         <div className="pride__card-one"></div>
--- src/components/WatchSelect/WatchSelect.js
+++ src/components/WatchSelect/WatchSelect.js
@@ -61,74 +61,82 @@ export default function WatchSelect() {
           </div>
         </div>
       </div>
-      <table className="select__table-container">
-        <tr className="select__table">
-          <td className="select__table-size">45mm or 41mm</td>
-          <td className="select__swim">Swimproof</td>
-          <td className="select__crack">Most crack-resistant front crystal</td>
-          <td className="select__dust">Certified IP6X dust resistant</td>
-        </tr>
-        <tr className="select__table">
-          <td className="select__table-size">44mm or 40mm</td>
-          <td className="select__swim">Swimproof</td>
-          <td className="select__crack">–</td>
-          <td className="select__dust">–</td>
-        </tr>
-        <tr className="select__table">
-          <td className="select__table-size">42mm or 38mm</td>
-          <td className="select__swim">Swimproof</td>
-          <td className="select__crack">–</td>
-          <td className="select__dust">–</td>
-        </tr>
-      </table>
-      <div className="select__display-container">
-        <div className="select__on">
-          <img alt=""></img>
-          <p className="select__on-header">Always-On Retina Display</p>
-          <p className="select__on-sub-header">
-            Nearly 20% larger than Apple Watch SE
-          </p>
-          <p className="select__on-size"></p>
+
+      <div className="select__table-container">
+        <div className="select__top-container">
+          <div className="select__table">
+            <p className="select__table-size">45mm or 41mm</p>
+            <p className="select__swim">Swimproof</p>
+            <p className="select__crack">Most crack-resistant front crystal</p>
+            <p className="select__dust">Certified IP6X dust resistant</p>
+          <div />
+          <div className="select__table">
+              <p className="select__table-size">44mm or 40mm</p>
+              <p className="select__swim">Swimproof</p>
+              <p className="select__crack">–</p>
+              <p className="select__dust">–</p>
+          </div>
+          <div className="select__table">
+              <p className="select__table-size">42mm or 38mm</p>
+              <p className="select__swim">Swimproof</p>
+              <p className="select__crack">–</p>
+              <p className="select__dust">–</p>
+          </div>
+          </div>
         </div>
-        <div className="select__retina">
-          <img alt=""></img>
-          <p className="select__retina-header">Retina display</p>
-          <p className="select__retina-sub-header">
-            Over 30% larger than Series 3
-          </p>
+
+        <div className="select__display-container">
+          <div className="select__on">
+            <img src="https://www.apple.com/v/watch/ax/images/compare/quicklook_retina_display_s7__euol4grkrx8i_large_2x.png" alt=""></img>
+            <p className="select__on-header">Always-On Retina Display</p>
+            <p className="select__on-sub-header">
+              Nearly 20% larger than Apple Watch SE
+            </p>
+            <p className="select__on-size"></p>
+          </div>
+          <div className="select__retina">
+            <img src="https://www.apple.com/v/watch/ax/images/compare/quicklook_retina_display_se__bkqpyec086ya_large_2x.png" alt=""></img>
+            <p className="select__retina-header">Retina display</p>
+            <p className="select__retina-sub-header">
+              Over 30% larger than Series 3
+            </p>
+          </div>
+          <div className="select__retina-display">
+            <img src="https://www.apple.com/v/watch/ax/images/compare/quicklook_retina_display_s3__c28bwxqba1w2_large_2x.png" alt=""></img>
+            <p className="select__retina-display-header">Retina display</p>
+          </div>
         </div>
-        <div className="select__retina-display">
-          <img alt=""></img>
-          <p className="select__retina-display-header">Retina display</p>
+
+        <div className="select__oxygen-container">
+          <div className="select__oxygen-on">
+            <img src="https://www.apple.com/v/watch/ax/images/compare/quicklook_bo_app__bn0uz5oly82u_large_2x.png" alt=""></img>
+            <p className="select__on-display-header">Blood Oxygen app</p>
+          </div>
+          <div className="select__retina">
+            <p className="select__retina-option">–</p>
+          </div>
+          <div className="select__retina-display">
+            <p className="select__retina-display-option">–</p>
+          </div>
         </div>
-      </div>
-      <div className="select__oxygen-container">
-        <div className="select__oxygen-on">
-          <img alt=""></img>
-          <p className="select__on-display-header">Blood Oxygen app</p>
+
+        <div className="select__ecg-container">
+          <div className="select__ecg-on">
+            <img src="https://www.apple.com/v/watch/ax/images/compare/quicklook_ecg_app__cha5dfocq4wi_large_2x.png" alt=""></img>
+            <p className="select__on-display-header">ECG app</p>
+          </div>
+          <div className="select__retina">
+            <p className="select__retina-option">–</p>
+          </div>
+          <div className="select__retina-display">
+            <p className="select__retina-display-option">–</p>
+          </div>
         </div>
-        <div className="select__retina">
-          <p className="select__retina-option">–</p>
+
+        <div className="select__compare-models">
+          <p className="select__compare">Compare all models</p>
+          <p className="select__compare-shop">Shop Apple Watch</p>
         </div>
-        <div className="select__retina-display">
-          <p className="select__retina-display-option">–</p>
-        </div>
-      </div>
-      <div className="select__ecg-container">
-        <div className="select__ecg-on">
-          <img alt=""></img>
-          <p className="select__on-display-header">ECG app</p>
-        </div>
-        <div className="select__retina">
-          <p className="select__retina-option">–</p>
-        </div>
-        <div className="select__retina-display">
-          <p className="select__retina-display-option">–</p>
-        </div>
-      </div>
-      <div className="select__compare-models">
-        <p className="select__compare">Compare all models</p>
-        <p className="select__compare-shop">Shop Apple Watch</p>
       </div>
     </section>
   );
