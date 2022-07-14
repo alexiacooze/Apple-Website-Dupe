4d22190f614649d53364dd65a644d88bb7b8377e
Alexia Cooze <alexiacooze@Alexias-MacBook-Air.local>
2022-07-13 22:20:52 -0400

    finished the top of the watch select componenet

--- src/components/WatchSelect/WatchSelect.js
+++ src/components/WatchSelect/WatchSelect.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../WatchSelect/WatchSelect.scss";
+import arrow from "../../assets/arrow-white.png";
 
 export default function WatchSelect() {
   return (
@@ -8,25 +10,55 @@ export default function WatchSelect() {
       </div>
       <div className="select__watches">
         <div className="select__seven-container">
-          <img alt="" className="select__image"></img>
-          <p className="select__header">Apple Watch Series 7</p>
+          <img
+            src="https://www.apple.com/v/watch/ax/images/compare/compare_s7__dqi9jnwa4sq6_large_2x.jpg"
+            alt=""
+            className="select__image"
+          ></img>
+          <p className="select__header-watch">Apple Watch Series 7</p>
           <p className="select__price">From $529</p>
           <button className="select__buy">Buy</button>
-          <p className="select__learn-more">Learn more {`>`}</p>
+          <div className="select__learn-container">
+            <p className="select__learn-more">Learn more</p>
+            <span>
+              {" "}
+              <img className="select__play" alt="play" src={arrow} />
+            </span>
+          </div>
         </div>
         <div className="select__se-container">
-          <img alt="" className="select__image"></img>
-          <p className="select__header">Apple Watch SE</p>
+          <img
+            src="https://www.apple.com/ca/watch/images/compare/compare_se__fjdos6x4rmmy_large_2x.jpg"
+            alt=""
+            className="select__image"
+          ></img>
+          <p className="select__header-watch">Apple Watch SE</p>
           <p className="select__price">From $369</p>
           <button className="select__buy">Buy</button>
-          <p className="select__learn-more">Learn more {`>`}</p>
+          <div className="select__learn-container">
+            <p className="select__learn-more">Learn more</p>
+            <span>
+              {" "}
+              <img className="select__play" alt="play" src={arrow} />
+            </span>
+          </div>
         </div>
         <div className="select__three-container">
-          <img alt="" className="select__image"></img>
-          <p className="select__header">Apple Watch Series 3</p>
+          <img
+            src="https://www.apple.com/v/watch/ax/images/compare/compare_s3__f6gqpfe5rdea_large_2x.jpg"
+            alt=""
+            className="select__image"
+          ></img>
+          <p className="select__header-watch">Apple Watch Series 3</p>
           <p className="select__price">From $249</p>
           <button className="select__buy">Buy</button>
-          <p className="select__learn-more">Learn more {`>`}</p>
+          <div className="select__learn-container">
+            <p className="select__learn-more">Learn more</p>
+            <span>
+              {" "}
+              <img className="select__play" alt="play" src={arrow} />
+            </span>
+          </div>
         </div>
       </div>
       <table className="select__table-container">
