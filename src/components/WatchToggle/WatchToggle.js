0398799563a4f2aa0a0096f122dd5e23a5b38ccd
Alexia Cooze <alexiacooze@Alexias-MacBook-Air.local>
2022-07-15 13:36:20 -0400

    finished the styling for the toggle componenent

--- src/components/WatchToggle/WatchToggle.js
+++ src/components/WatchToggle/WatchToggle.js
@@ -2,16 +2,17 @@ import React, { useState } from "react";
 import "../WatchToggle/WatchToggle.scss";
 
 export default function WatchToggle() {
-  // will need to use state to toggle with an onlick that changes the color make the whole div the grey color and then make the onlick balck with rounded borders
+  const [active, setActive] = useState(false);
 
-
-
-  const [toggle, setToggle] = useState();
-
-  console.log(toggle);
-  const changeToggle = () => {
-    setToggle("toggle__switch-seven")  
+  const toggle = () => {
+    active ? setActive(false) : setActive(true);
   };
+
+  // const [toggleChange, setToggleChange] = useState(false);
+
+  // const toggled = () => {
+  //   toggleChange ? setToggleChange(false) : setToggleChange(true);
+  // };
 
   return (
     <section className="toggle">
@@ -33,14 +34,46 @@ export default function WatchToggle() {
           <p className="toggle__header">
             Over 50% more screen area than Series 3.
           </p>
+          <div className="toggle__grey">
+            <p className="toggle__grey-text">div</p>
+          </div>
+          <div className="toggle__series-container">
+            <p className="toggle__series-7">Series 7</p>
+            <p className="toggle__series-3">Series 3</p>
+          </div>
+          <div className="toggle__black-container">
+            <p onClick={toggle} className="toggle__black">
+             {active ? <span>Series 3</span> : <span>Series 7</span>}
+            </p>
+          </div>
           <div className="toggle__container">
-            <button onClick={changeToggle}
-              
-              className="toggle__seven toggle__switch-seven"
-            >
-              Series 7
-            </button>
-            <button className="toggle__three toggle__switch-three">Series 3</button>
+            {/* {toggle ? (
+              <button
+                onClick={toggle}
+                className={`"toggle__btn" ${
+                  active ? "toggle__btn" : "toggle__seven"
+                }`}
+              >
+                {" "}
+                {active ? <span>Series 7</span> : <span>Series 7</span>}
+              </button>
+            ) : (
+              ""
+            )}
+
+            {toggle ? (
+              <button
+                onClick={toggled}
+                className={`"toggle__btns" ${
+                  toggleChange ? "toggle__btns" : "toggle__three"
+                }`}
+              >
+                {" "}
+                {toggleChange ? <span>Series 3</span> : <span>Series 3</span>}
+              </button>
+            ) : (
+              ""
+            )} */}
           </div>
         </div>
       </div>
